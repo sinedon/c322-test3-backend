542ff3fb7464adef31816175c2dc51efff2e6f50
sinedon <111921789+sinedon@users.noreply.github.com>
2024-04-21 19:41:24 -0400

    Added some fixes.

--- src/main/java/edu/iu/c322/test3/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/c322/test3/controllers/AuthenticationController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+
 @RestController
+@CrossOrigin
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final IAuthenticationService authenticationService;
